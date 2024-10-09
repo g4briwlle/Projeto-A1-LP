1c7bb25cd498117d6cb6a297dbf21306c279f87c
Vinicio Deusdara <viniciodeusdara@gmail.com>
2024-10-09 18:55:17 -0300

    Coloquei código do streamlit no arquivo main.py

--- src/happiscore_x_democracy.py
+++ src/happiscore_x_democracy.py
@@ -12,22 +12,6 @@ cl.df_hapiscore_limpo.fillna(method="ffill", inplace=True)
 cl.df_democracy_limpo.fillna(method="ffill", inplace=True)
 
 
-st.set_page_config(page_title="Análise de Felicidade e Democracia", layout="wide")
-
-# Adicionando CSS
-st.markdown("""
-    <style>
-    .main {
-        color: white;
-    }
-    .stPlotlyChart {
-        background-color: #161A28;
-        border-radius: 15px;
-        padding: 10px;
-    }
-    </style>
-    """, unsafe_allow_html=True)
-
 # Função para plotar os gráficos
 def plot_happiness_democracy(year):
     # Dados para o ano selecionado
@@ -105,22 +89,3 @@ def plot_happiness_democracy(year):
                 autoexpand=False),
         )
         st.plotly_chart(democracy_map, use_container_width=True)
-
-# Barra lateral para seleção de análise
-st.sidebar.title("Navegação")
-analise_selecionada = st.sidebar.selectbox("Selecione a análise", ["Happiness vs Democracy", "OUTRAS ANÁLISES"])
-
-# Slider de ano
-ano = st.sidebar.slider(
-    "Selecione o ano",
-    min_value=int(min(cl.df_hapiscore_limpo.columns[1:])), 
-    max_value=int(max(cl.df_hapiscore_limpo.columns[1:])),
-    value=int(min(cl.df_hapiscore_limpo.columns[1:])),
-    step=1)
-
-
-if analise_selecionada == "Happiness vs Democracy":
-    plot_happiness_democracy(ano)
-else:
-    st.title("Outras Correlações")
-    st.write("Colocar outras análises aqui")
--- src/main.py
+++ src/main.py
@@ -1,2 +1,40 @@
 """
 """
+from happiscore_x_democracy import plot_happiness_democracy
+import streamlit as st
+import cleaning as cl
+
+st.set_page_config(page_title="Análise de Felicidade e Democracia", layout="wide")
+
+# Adicionando CSS
+st.markdown("""
+    <style>
+    .main {
+        color: white;
+    }
+    .stPlotlyChart {
+        background-color: #161A28;
+        border-radius: 15px;
+        padding: 10px;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Barra lateral para seleção de análise
+st.sidebar.title("Navegação")
+analise_selecionada = st.sidebar.selectbox("Selecione a análise", ["Happiness vs Democracy", "OUTRAS ANÁLISES"])
+
+# Slider de ano
+ano = st.sidebar.slider(
+    "Selecione o ano",
+    min_value=int(min(cl.df_hapiscore_limpo.columns[1:])), 
+    max_value=int(max(cl.df_hapiscore_limpo.columns[1:])),
+    value=int(min(cl.df_hapiscore_limpo.columns[1:])),
+    step=1)
+
+
+if analise_selecionada == "Happiness vs Democracy":
+    plot_happiness_democracy(ano)
+else:
+    st.title("Outras Correlações")
+    st.write("Colocar outras análises aqui")
